7a18ecf33fab95925850de1c608c3c1568d8aeca
Skryptonyte <rayhan.faizel@gmail.com>
2021-11-27 10:02:27 +0530

    Some code cleanup

--- vegitotweakprefbundle/VEGITOPREFRootListController.m
+++ vegitotweakprefbundle/VEGITOPREFRootListController.m
@@ -24,28 +24,4 @@
 	waitpid(pid, &status, WEXITED);
 }
 
-- (void) doFilePick
-{
-	UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
-	imagePicker.delegate = self;
-	imagePicker.allowsEditing = NO;
-	imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-    imagePicker.mediaTypes = [[NSArray alloc] initWithObjects: (NSString *) kUTTypeMovie, nil];
-
-    [self presentViewController:imagePicker animated:YES completion:nil];
-}
-
-- (void)imagePickerController:(UIImagePickerController *)picker 
-didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey, id> *)info;
-{
-	NSURL* url = info[UIImagePickerControllerMediaURL];
-	NSLog(@"%@",url.absoluteString);
-
-	NSURL* newurl = [NSURL fileURLWithPath: [NSString stringWithFormat:@"/var/mobile/%@", url.lastPathComponent]];
-
-	[[NSFileManager defaultManager] copyItemAtURL:url toURL:newurl error: nil];
-
-	[[NSUserDefaults standardUserDefaults] setURL:newurl forKey: @"url"];
-	[picker dismissViewControllerAnimated:YES completion: NULL];
-}
 @end
